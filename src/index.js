0ac5b0a31561cffa074a79cdb6d2ee984f95abe0
rtroe-xps <rob.tom.roe@gmail.com>
2022-02-12 08:06:22 -0800

    Added logging

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const app = express();
 
 let procs = [];
 
+let logs = [];
+
 let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 // console.log(config);
@@ -18,45 +20,46 @@ let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 let platform = config[os.platform()];
 
-function Refresh()
-{
-    console.log("Pulling latest");
-    
-    procs.forEach(inst=>{
+function Refresh() {
+
+    procs.forEach(inst => {
         console.log("Killing " + inst.p.pid);
         inst.p.kill();
     });
-    
-    execSync(`rm -r ./metric-netplay-server`);
-    
+
+   // execSync(`rm -r ./metric-netplay-server`);
+    console.log("Pulling latest");
+
     clone(config.repo, './metric-netplay-server', [], function () {
         console.log("done");
-        
+
         const filePath = `${process.cwd()}/metric-netplay-server/${platform.proc_path}/${platform.proc_name}`;
-    
+
         console.log("setting permissons")
         execSync(`chmod +x ${filePath}`);
         console.log("done");
-    
+
         for (let i = 1; i < platform.proc_count + 1; i++) {
-    
+
             let proc = {};
-            proc["name"]=os.hostname() + `-${i}`;
-            proc["port"]=`1424${i + 1}`;
-            
+            proc["name"] = os.hostname() + `-${i}`;
+            proc["port"] = `1424${i + 1}`;
+
             const child = spawn(filePath, ['-n', proc["name"], '-p', proc["port"]]);
-    
+
             child.stdout.on('data', data => {
                 console.log(`stdout${i}:${data}`);
+                logs.push(`stdout${i}:${data}`);
             });
-    
+
             child.stderr.on('data', data => {
                 console.error(`stderr${i}: ${data}`);
+                logs.push(`stderr${i}:${data}`);
             });
-    
-            proc["pid"]=child.pid;
-            proc["p"]=child;
-            
+
+            proc["pid"] = child.pid;
+            proc["p"] = child;
+
             procs.push(proc);
         }
     });
@@ -64,39 +67,46 @@ function Refresh()
 
 
 app.get('/', (req, res) => {
-    
+
     var resObj = {
-        name:"test server",
-        version: "0.1.3",
+        name: "test server",
+        version: "0.1.4",
         uptime: os.uptime(),
-        processes:procs
+        processes: procs
     }
     res.send(resObj);
-    
-  });
 
-  
+});
+
+
 app.get('/procs', (req, res) => {
-    
-    res.send(procs);
-  });
 
-  app.get('/reboot', (req, res) => {
-      
-  execSync(`reboot`);
-    });
-  
-  
+    res.send(procs);
+});
+
+app.get('/reboot', (req, res) => {
+
+    execSync(`systemctl start reboot.target`);
+});
+
+
 app.get('/refresh', (req, res) => {
-    
-   Refresh();  
-   console.log("Finished refresh")
-    res.send(procs);
-  });
-  
-  app.listen(3000, () =>{
 
-   console.log('Example app is listening on port 3000.');
-   Refresh();  
+    Refresh();
+    console.log("Finished refresh")
+    res.send({
+        status:"refreshing..."
+    });
+});
+
+
+app.get('/logs', (req, res) => {
+    res.send(logs);
+});
+
+app.listen(3000, () => {
+
+    console.log('App is listening on port 3000.');
+    Refresh();
 
 });
